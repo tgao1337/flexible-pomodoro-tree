96addaab10bd436e69acac150e0c31df1c478745
Tommy Gao <gaotommy10@gmail.com>
2022-02-16 12:15:00 -0500

    added buzzer functions to different time+freq #30

--- buzzer.py
+++ buzzer.py
@@ -1,17 +1,30 @@
 import pigpio
 import time
 
-pi = pigpio.pi()
-# first arg is pin number,
-# second arg is frequency in Hz,
-# third arg is number of ON units out of 1000000
-pi.hardware_PWM(13, 4000, 450000) 
-time.sleep(2)
-pi.hardware_PWM(13, 4000, 500000)
-time.sleep(2)
-pi.hardware_PWM(13, 4000, 550000)
-time.sleep(2)
+def playTime(seconds):
+  pi = pigpio.pi()
+  # first arg is pin number,
+  # second arg is frequency in Hz,
+  # third arg is number of ON units out of 1000000
+  pi.hardware_PWM(13, 4000, 500000) 
+  time.sleep(seconds)
+  pi.hardware_PWM(13, 4000, 0)
+  
+def playFreq(freq):
+  pi = pigpio.pi()
+  # first arg is pin number,
+  # second arg is frequency in Hz,
+  # third arg is number of ON units out of 1000000
+  pi.hardware_PWM(13, freq, 500000) 
+  time.sleep(0.5)
+  pi.hardware_PWM(13, freq, 0)
+  
+def playFreqTime(freq, seconds):
+  pi = pigpio.pi()
+  # first arg is pin number,
+  # second arg is frequency in Hz,
+  # third arg is number of ON units out of 1000000
+  pi.hardware_PWM(13, freq, 500000) 
+  time.sleep(seconds)
+  pi.hardware_PWM(13, freq, 0)
 
-
-pi.hardware_PWM(13, 4000, 0)
-time.sleep(1)
