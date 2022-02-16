10556c2174beaf4f0f7ede0cfa0471fac3a26664
Tommy Gao <gaotommy10@gmail.com>
2022-02-16 00:57:02 -0500

    #29 added lines to test different  tones

--- buzzer.py
+++ buzzer.py
@@ -5,6 +5,13 @@ pi = pigpio.pi()
 # first arg is pin number,
 # second arg is frequency in Hz,
 # third arg is number of ON units out of 1000000
-pi.hardware_PWM(13, 4000, 500000) 
-
+pi.hardware_PWM(13, 4000, 450000) 
 time.sleep(2)
+pi.hardware_PWM(13, 4000, 500000)
+time.sleep(2)
+pi.hardware_PWM(13, 4000, 550000)
+time.sleep(2)
+
+
+pi.hardware_PWM(13, 4000, 0)
+time.sleep(1)
