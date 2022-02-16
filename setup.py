ba5db0dd878ffffee6fe68ef0212c83dd581ae67
Tommy Gao <gaotommy10@gmail.com>
2022-02-16 01:04:12 -0500

    #14 started setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pomodorotree',
+	version='1.0',
+	description='Flexible Pomodoro Tree for productivity',
+	author='Tommy Gao, Shahzeb Naseer, Nick Coluccio',
+	author_email='tommygao@nyu.edu',
+	url='https://github.com/tgao1337/flexible-pomodoro-tree',
+	install_requires=['smbus'],
+	py_modules=['pomodorotree'],
+       )
