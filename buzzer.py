d99711ab91f76580390df5dfa2c4dbe2209f02de
Tommy Gao <gaotommy10@gmail.com>
2022-02-21 22:56:11 -0500

    added commeents of how functions work #30

--- buzzer.py
+++ buzzer.py
@@ -2,6 +2,8 @@ import pigpio
 import time
 
 def playTime(seconds):
+  # This will play the buzzer at the default 4kHz for 
+  # (seconds) amount of seconds.
   pi = pigpio.pi()
   # first arg is pin number,
   # second arg is frequency in Hz,
@@ -11,6 +13,7 @@ def playTime(seconds):
   pi.hardware_PWM(13, 4000, 0)
   
 def playFreq(freq):
+  # This will play buzzer at a given frequency for 0.5 seconds.
   pi = pigpio.pi()
   # first arg is pin number,
   # second arg is frequency in Hz,
@@ -20,6 +23,7 @@ def playFreq(freq):
   pi.hardware_PWM(13, freq, 0)
   
 def playFreqTime(freq, seconds):
+  # This function plays a specified frequency for a specified time.
   pi = pigpio.pi()
   # first arg is pin number,
   # second arg is frequency in Hz,
