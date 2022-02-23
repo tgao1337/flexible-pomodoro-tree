426c681625fd7b764dc82c76a742b862320784f2
Shahzeb Naseer <shahzebnaseer@live.com>
2022-02-23 17:35:06 -0500

    Cleaned up code for SSD1106.py

--- SSD1106.py
+++ SSD1106.py
@@ -19,31 +19,29 @@ class SSD1106(object):
     def setup(self):
         # Initialize display
         self.write(0xAE)	# Turn off OLED panel
-        self.write(0x02)	# Set low column address
-        self.write(0x10)	# Set high column address
-        self.write(0x40)	# Set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
-        self.write(0x81)	# Set contrast control register
-        self.write(0xA0)	# Set SEG/Column Mapping     
-        self.write(0xC0)	# Set COM/Row Scan Direction   
-        self.write(0xA6)	# Set normal display
-        self.write(0xA8)	# Set multiplex ratio(1 to 64)
-        self.write(0x3F)	# 1/64 duty
-        self.write(0xD3)	# Set display offset    Shift Mapping RAM Counter (0x00~0x3F)
-        self.write(0x00)	# Not offset
-        self.write(0xd5)	# Set display clock divide ratio/oscillator frequency
-        self.write(0x80)	# Set divide ratio, Set Clock as 100 Frames/Sec
-        self.write(0xD9)	# Set pre-charge period
-        self.write(0xF1)	# Set Pre-Charge as 15 Clocks & Discharge as 1 Clock
-        self.write(0xDA)	# Set com pins hardware configuration
-        self.write(0x12)
-        self.write(0xDB)	# Set vcomh
-        self.write(0x40)	# Set VCOM Deselect Level
-        self.write(0x20)	# Set Page Addressing Mode (0x00/0x01/0x02)
-        self.write(0x02)
-        self.write(0xA4)	# Disable Entire Display On (0xa4/0xa5)
-        self.write(0xA6)	# Disable Inverse Display On (0xa6/a7) 
+        self.write(0x02)	# Set Lower Column Address
+        self.write(0x10)	# Set Higher Column address
+        self.write(0x40)	# Set Display Start Line
+        self.write(0x81)	# Set Contrast Control Mode
+        self.write(0xA0)	# Set Segment Re-map
+        self.write(0xC0)	# Set Common Output Scan Direction
+        self.write(0xA6)	# Set Normal Display
+        self.write(0xA8)	# Set Multiplex Ration Mode
+        self.write(0x3F)	# Set Multiplex Ration Data: ratio 64
+        self.write(0xD3)	# Set Display Offset Mode
+        self.write(0x00)	# Set Display Offset Data: 0
+        self.write(0xD5)	# Set Divide Ratio/Oscillator Frequency Mode
+        self.write(0x80)	# Set Divide Ratio/Oscillator Frequency Data
+        self.write(0xD9)	# Set Pre-charge Period Mode
+        self.write(0xF1)	# Set Pre-Charge Period As 15 Clocks & Discharge Period as 1 Clock
+        self.write(0xDA)	# Set Common Pads Hardware Configuration Mode
+        self.write(0x12)        # Set Sequential/Alternative Mode
+        self.write(0xDB)	# Set VCOM Deselect Level Mode Set
+        self.write(0x40)	# Set VCOM Deselect Level Data
+        self.write(0x20)	# Set Page Addressing Mode (0x00/0x01/0x02) -----
+        self.write(0xA4)	# Set Entire Display Off
         time.sleep(0.1)
-        self.write(0xAF)	# Turn on oled panel
+        self.write(0xAF)	# Turn on OLED panel
 
 
     def getbuffer(self, image):
@@ -53,10 +51,8 @@ class SSD1106(object):
         pixels = image_monocolor.load()
 
         if(imwidth == self.width and imheight == self.height):
-
             for y in range(imheight):
                 for x in range(imwidth):
-
                     if pixels[x, y] == 0:
                         buf[x + (y // 8) * self.width] &= ~(1 << (y % 8))
 
@@ -71,20 +67,12 @@ class SSD1106(object):
 
     def ShowImage(self, pBuf):
         for page in range(0,8):
-            # set page address #
             self.write(0xB0 + page);
-            # set low column address #
-            self.write(0x02); 
-            # set high column address #
-            self.write(0x10); 
-            # write data #
-            # time.sleep(0.01)
-            for i in range(0,self.width):#for(int i=0;i<self.width; i++)
-                #config.i2c_writebyte(0x40, ~pBuf[i+self.width*page])
+            self.write(0x02);
+            self.write(0x10);
+            for i in range(0, self.width):
                 self.bus.write_byte_data(self.address, 0x40, ~pBuf[i+self.width*page])
 
-
     def clear(self):
-        """Clear contents of image buffer"""
         _buffer = [0xff]*(self.width * self.height//8)
         self.ShowImage(_buffer)
