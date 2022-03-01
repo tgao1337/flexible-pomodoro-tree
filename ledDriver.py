c237d53b4022b1bd257eeab4d7f663fd182472a0
Nicholas Coluccio <ncoluccio327@gmail.com>
2022-03-01 00:38:19 -0500

    Updated led driver status to dict and changed logic of code #39

--- ledDriver.py
+++ ledDriver.py
@@ -14,15 +14,21 @@ SRCLR= 23
 
 NUM_CLUSTERS=4
 NUM_COLORS=3
-'''An array that of 12 integers that will account whether a given color group of LEDs is lit on a given cluster 
- the first three indices correspond to cluster 1, red, yellow , green and so on. 0 means offf, 1 is on. '''
-clusterStatus=[0]*(NUM_CLUSTERS*NUM_COLORS)
 
-# Global variables that will be passed into the drivers to light up a specfic color group.
-GREEN_YELLOW_RED="01111111"
-YELLOW_RED="00001111"
-RED="00000001"
-OFF= "00000000"
+ledDict= {}
+ledDict["r1"]=[0]
+ledDict["y1"]=[0,0,0]
+ledDict["g1"]=[0,0,0]
+ledDict["r2"]=[0]
+ledDict["y2"]=[0,0,0]
+ledDict["g2"]=[0,0,0]
+ledDict["r3"]=[0]
+ledDict["y3"]=[0,0,0]
+ledDict["g3"]=[0,0,0]
+ledDict["r4"]=[0]
+ledDict["y4"]=[0,0,0]
+ledDict["g4"]=[0,0,0]
+
 
 #setup the pins as outputs and set the clocks initally to low and the clear initially to high. 
 def setup():
@@ -34,7 +40,7 @@ def setup():
   GPIO.output(RCLK, GPIO.LOW)
   GPIO.output(SRCLK, GPIO.LOW)
   GPIO.output(SRCLR, GPIO.HIGH)
-  clearAll()
+
 #using the commonly connected SRCLR to all the drivers, pulsing RCLK while this is 0  will clear all drivers and LEDs oFF?.
 def clearAll():
   GPIO.output(SRCLR, GPIO.LOW)
@@ -43,8 +49,11 @@ def clearAll():
   GPIO.output(RCLK, GPIO.HIGH)
   GPIO.output(SRCLR, GPIO.HIGH)
 
-  for i in range(NUM_COLORS *NUM_CLUSTERS):
-   clusterStatus[i]=0
+  for key in ledDict:
+   if "r" in key:
+      ledDict[key]=[0]
+   else:
+      ledDict[key]=[0,0,0]
 
 '''Given a certain string of 1010, this function will set the serial input and pulse the SRCLK and RCLK so the 
 given pattern from the string is displayed corrrectly on the LEDS
@@ -66,226 +75,233 @@ def display():
   GPIO.output(RCLK, GPIO.HIGH)
 
 '''This function allows you to turn on a given color group of a given cluster
-Arguements: int cluster (1-4), char color( r,y,g)'''
-def selectClusterColorON( cluster, color):
-  #get current status of the LEDs
-  cluster1= readClusterStatus(1)
-  cluster2=readClusterStatus(2)
-  cluster3=readClusterStatus(3)
-  cluster4= readClusterStatus(4)
-  newCluster=""
+Arguements: int cluster (1-4), char color( r,y,g), bool on or off '''
+def toggleClusterColorGroup( cluster,color,turnOn):
 
-  #determine new string that will be passed into the desire cluster based on the color desired. 
-  if(color=='g'):
-   newCluster=GREEN_YELLOW_RED
-  elif (color == 'y'):
-   newCluster= YELLOW_RED
-  elif (color == 'r'):
-   newCluster=RED
+  if (color != 'r' and color != 'y' and color != 'g'):
+    raise Exception("invalid color selection in ClusterColorON(), choose'g', 'y', 'r' ")
+  if(cluster <1  or  cluster >NUM_CLUSTERS):
+    raise Exception("Number clusters invalid, must be an int>0 and less than NUM_CLUSTERS=",NUM_CLUSTERS)
+  if(turnOn):
+   updateClusterStatus(cluster,True,color,True)
   else:
-    raise Exception("invalid color selection in selectClusterON(), choose g, y, r ")
-  #keep the other clusters the same while updating the desired cluster LEDS
-  if(cluster ==1):
-    passToLEDs(cluster4)
-    passToLEDs(cluster3)
-    passToLEDs(cluster2)
-    passToLEDs(newCluster)
-  elif(cluster ==2):
-    passToLEDs(cluster4)
-    passToLEDs(cluster3)
-    passToLEDs(newCluster)
-    passToLEDs(cluster1)
-  elif(cluster ==3):
-    passToLEDs(cluster4)
-    passToLEDs(newCluster)
-    passToLEDs(cluster2)
-    passToLEDs(cluster1)
-  elif(cluster ==4):
-    passToLEDs(newCluster)
-    passToLEDs(cluster3)
-    passToLEDs(cluster2)
-    passToLEDs(cluster1)
-  else:
-     raise Exception("Number clusters invalid, must be an int>0 and less than NUM_CLUSTERS=",NUM_CLUSTERS)
-# update the status array
-  updateClusterStatus(cluster, True, color)
+   updateClusterStatus(cluster,True,color,False)
+
+  for i in range(NUM_CLUSTERS,0,-1):
+   passToLEDs(readClusterStatus(i))
+
   display()
 
-'''This function allows you to turn off a given color group of a given cluster
-Arguements: int cluster (1-4), char color( r,y,g)'''
-def selectClusterColorOFF( cluster, color):
-  #get current status of the LEDs
-  cluster1= readClusterStatus(1)
-  cluster2=readClusterStatus(2)
-  cluster3=readClusterStatus(3)
-  cluster4= readClusterStatus(4)
-  oldCluster=readClusterStatus(cluster)
+'''This function allows you to turn on the lowest currently off led in the chain or turn off the highedst currently on led in the chain
+Arguements: bool on or off'''
+def toggleNextLed(turnOn):
 
-  #determine new string that will be passed into the desire cluster based on the color desired off.
-  if(color=='g'):
-   newCluster=oldCluster[0]+"000"+oldCluster[4:]
-  elif (color == 'y'):
-   newCluster= oldCluster[:4]+"000"+oldCluster[7]
-  elif (color == 'r'):
-   newCluster=oldCluster[:7]+"0"
+  if(turnOn):
+    if(not (findNextLedToUpdateOn())):
+     print("ALL LEDS ON ALREADY\n")
   else:
-    raise Exception("invalid color selection in selectClusterON(), choose g, y, r ")
-  #keep the other clusters the same while updating the desired cluster LEDS
-  if(cluster ==1):
-    passToLEDs(cluster4)
-    passToLEDs(cluster3)
-    passToLEDs(cluster2)
-    passToLEDs(newCluster)
-  elif(cluster ==2):
-    passToLEDs(cluster4)
-    passToLEDs(cluster3)
-    passToLEDs(newCluster)
-    passToLEDs(cluster1)
-  elif(cluster ==3):
-    passToLEDs(cluster4)
-    passToLEDs(newCluster)
-    passToLEDs(cluster2)
-    passToLEDs(cluster1)
-  elif(cluster ==4):
-    passToLEDs(newCluster)
-    passToLEDs(cluster3)
-    passToLEDs(cluster2)
-    passToLEDs(cluster1)
-  else:
-     raise Exception("Number clusters invalid, must be an int>0 and less than NUM_CLUSTERS=",NUM_CLUSTERS)
-# update the status array
-  updateClusterStatus(cluster, False, color)
+   if(not(findNextLedToUpdateOff())):
+    print("ALL LEDS OFF ALREADY\n")
+
+  for i in range(NUM_CLUSTERS,0,-1):
+   passToLEDs(readClusterStatus(i))
+
   display()
+'''This function (not to be called) will update the ledDict based on what led/s have turned on/off. 
+If we wanted we could call any led from here to see if there is something wrong with it. 
+Arugments: int cluster, bool turnWholeClusterOn, char color, bool turnLedOn'''
+def updateClusterStatus(cluster,turnWholeClusterOn,color,turnLedOn):
+  if (color != 'r' and color != 'y' and color != 'g'):
+   raise Exception( "Need to pass r,y,g in updateDictionary()")
 
+  key= color + str(cluster)
 
-'''This  function keeps track of changes made to  the clusters by updating the status array so that it is accurate.
-Arugmetns: int cluster, bool on/off (T/F), char color( r,y,g,x) '''
-def updateClusterStatus(cluster, on_off, color='x'):
-  # find cluster's position in the array
-  index=0
-  index +=(cluster-1)*3
-#this happens when updating an entire cluster to turn off, since I only pass in two parameters from the OFF() funcction. 
-  if(color== 'x'):
-   clusterStatus[index]=0
-   clusterStatus[index+1]=0
-   clusterStatus[index+2]=0
-  else:  #updating a cluster with a new color being turned on or off
-    if (color =='r'):
-      pass     #the index is already in the position for the r index
-    elif(color =='y'):
-      index+=1
-    elif(color =='g'):
-      index+=2
+  if (color=='r'):
+    if (turnLedOn):
+       ledDict[key]=[1]
     else:
-      raise Exception("color error in updateCLusterStatus() use r,y,g")
-    if(on_off):
-      clusterStatus[index]=1
-    else:
-      clusterStatus[index]=0
+      ledDict[key]=[0]
 
-'''This function iterates its way through the status array and generates an 8 bit binary 
-representation of the actual LED sequence to be passed in to the seriel input of driver
-Arugment: int cluster                 Returns: binary string'''
+  elif(turnWholeClusterOn):  # entire cluster is turned off or on
+    if(turnLedOn):
+      ledDict[key]=[1,1,1]
+    else:
+      ledDict[key]=[0,0,0]
+
+  else:  # find the led that needs to be updated on or off 
+    if(turnLedOn):
+       findNextLedToUpdateOn(key)
+    else:
+       findNextLedToUpdateOff(key)
+'''This is a recursive function which finds the first 0 in the LedDict and then updates the dict to turn that 
+LED on. It starts at the first driver, which would be the lowest physically on the tree and looks to see if any
+leds in any color groups are off and then moves onto the next one and next one.'''
+def findNextLedToUpdateOn(key="x"):
+  if (key != "x"): 
+   found=False
+   arr=ledDict.get(key)
+   for i in range(3):
+      if(arr[i]==0):
+        arr[i]=1
+        found=True
+        break
+   ledDict[key]=arr
+   return found
+  else:
+   for i in range (1,NUM_CLUSTERS+1):
+      redKey = "r"+str(i)
+      yelKey= "y"+str(i)
+      grKey= "g" + str(i)
+      if(ledDict.get(redKey)[0]==0):
+         ledDict[redKey]=[1]
+         return True
+      if(findNextLedToUpdateOn(yelKey)):
+          return True
+      if(findNextLedToUpdateOn(grKey)):
+          return True
+   return False
+
+'''This is a recursive function which finds the first 1 in the LedDict and then updates the dict to turn that
+LED off. It starts at the fourth driver, which would be the highest physically on the tree and looks to see if any
+leds in any color groups are on and then moves onto the lower one and so on.'''
+def findNextLedToUpdateOff(key="x"):
+  if (key !="x"):
+   found=False
+   arr=ledDict.get(key)
+   for i in range( 2, -1, -1):
+        if (arr[i]==1):
+          arr[i]=0
+          found=True
+          break
+   ledDict[key]=arr
+   return found
+  else:
+   for i in range (NUM_CLUSTERS,0,-1):
+      redKey = "r"+str(i)
+      yelKey= "y"+str(i)
+      grKey= "g" + str(i)
+      
+      if(findNextLedToUpdateOff(grKey)):
+          return True
+      if(findNextLedToUpdateOff(yelKey)):
+          return True
+      if(ledDict.get(redKey)[0]==1):
+         ledDict[redKey]=[0]
+         return True
+   return False
+'''This function reads the ledDict and generates an 8 bit binary string for the current state of the 
+LEDs. The string is the returned in the reverse since the drivers need to be inputted that way
+Arguments: int cluster'''
 def readClusterStatus(cluster):
-  start=(cluster-1)*3
+  redKey = "r"+str(cluster)
+  yelKey= "y"+str(cluster)
+  grKey= "g" + str(cluster)
 
-  if(clusterStatus[start+2]==1): 
-   return GREEN_YELLOW_RED
-  elif(clusterStatus[start+1]==1):
-   return YELLOW_RED
-  elif(clusterStatus[start]==1):
-   return RED
-  else:
-   return OFF
+  status=""
+  status += str(ledDict.get(redKey)[0])
+
+  for led in ledDict.get(yelKey):
+    status +=str(led)
+ 
+  for led in ledDict.get(grKey):
+    status +=str(led)
+
+  status +="0"
+
+  return status[::-1]
+
 
 '''This function will turn off an entire cluster no matter which LEDS are currently on 
 Arguments: int cluster'''
 def selectClusterOFF(cluster):
-  cluster1= readClusterStatus(1)
-  cluster2=readClusterStatus(2)
-  cluster3=readClusterStatus(3)
-  cluster4= readClusterStatus(4)
-  newCluster=OFF
 
-  #keep the other clusters the same while updating the desired cluster  OFF
-  if(cluster ==1):
-    passToLEDs(cluster4)
-    passToLEDs(cluster3)
-    passToLEDs(cluster2)
-    passToLEDs(newCluster)
-  elif(cluster ==2):
-    passToLEDs(cluster4)
-    passToLEDs(cluster3)
-    passToLEDs(newCluster)
-    passToLEDs(cluster1)
-  elif(cluster ==3):
-    passToLEDs(cluster4)
-    passToLEDs(newCluster)
-    passToLEDs(cluster2)
-    passToLEDs(cluster1)
-  elif(cluster ==4):
-    passToLEDs(newCluster)
-    passToLEDs(cluster3)
-    passToLEDs(cluster2)
-    passToLEDs(cluster1)
-  else:
-     raise Exception("Number clusters invalid, must be an int>0 and less than NUM_CLUSTERS=",NUM_CLUSTERS)
+  if(cluster <1 or cluster >NUM_CLUSTERS):
+   raise Exception("Number clusters invalid, must be an int>0 and less than NUM_CLUSTERS=",NUM_CLUSTERS)
+  
+  updateClusterStatus(cluster,True,'r',False)
+  updateClusterStatus(cluster,True,'y',False)
+  updateClusterStatus(cluster,True,'g',False)
 
-  updateClusterStatus(cluster, False)
+  for i in range(NUM_CLUSTERS,0,-1):
+   passToLEDs(readClusterStatus(i))
+
   display()
+'''This function turns on all LEDs'''
+def allOn():
 
-#test code 
+  
+  for i in range(NUM_CLUSTERS,0,-1):
+   passToLEDs("11111111")
+  
+  display()
+  time.sleep(2)
 
+#test code
 if __name__ == '__main__':
   setup()
-  selectClusterColorON(1,'g')
-  time.sleep(0.3)
-  selectClusterColorON(2,'g')
-  time.sleep(0.3)
-  selectClusterColorON(3,'g')
-  time.sleep(0.3)
-  selectClusterOFF(2)
-  time.sleep(0.3)
-  selectClusterOFF(3)
-  time.sleep(0.3)
+
+  allOn()
+  time.sleep(0.05)  
   clearAll()
+  print(ledDict)
+  
+  for i in range(22):
+   toggleNextLed(True)
+  clearAll()
+  print(ledDict)
+  for i in range(22):
+   toggleNextLed(False)
 
-  #simulating the tree lighting up fully and then dimming fully 
-  selectClusterColorON(1,'r')
-  time.sleep(0.3)
-  selectClusterColorON(2,'r')
-  time.sleep(0.3)
-  selectClusterColorON(3,'r')
-  time.sleep(0.3)
-  selectClusterColorON(1,'y')
-  time.sleep(0.3)
-  selectClusterColorON(2,'y')
-  time.sleep(0.3)
-  selectClusterColorON(3,'y')
-  time.sleep(0.3)
-  selectClusterColorON(1,'g')
-  time.sleep(0.3)
-  selectClusterColorON(2,'g')
-  time.sleep(0.3)
-  selectClusterColorON(3,'g')
-  time.sleep(0.3)
 
-# now starting to dim 
-  selectClusterColorOFF(1,'g')
-  time.sleep(0.3)
-  selectClusterColorOFF(2,'g')
-  time.sleep(0.3)
-  selectClusterColorOFF(3,'g')
-  time.sleep(0.3)
-  selectClusterColorOFF(1,'y')
-  time.sleep(0.3)
-  selectClusterColorOFF(2,'y')
-  time.sleep(0.3)
-  selectClusterColorOFF(3,'y')
-  time.sleep(0.3)
-  selectClusterColorOFF(1,'r')
-  time.sleep(0.3)
-  selectClusterColorOFF(2,'r')
-  time.sleep(0.3)
-  selectClusterColorOFF(3,'r')
-  time.sleep(0.3)
+  toggleClusterColorGroup(1,'r',True)
+  toggleClusterColorGroup(2,'r',True)
+  toggleClusterColorGroup(3,'r',True)
+  toggleClusterColorGroup(4,'r',True)
+  print(ledDict)
+
+  toggleClusterColorGroup(1,'y',True)
+  toggleClusterColorGroup(2,'y',True)
+  toggleClusterColorGroup(3,'y',True)
+  toggleClusterColorGroup(4,'y',True)
+  print(ledDict)
+
+  toggleClusterColorGroup(1,'g',True)
+  toggleClusterColorGroup(2,'g',True)
+  toggleClusterColorGroup(3,'g',True)
+  toggleClusterColorGroup(4,'g',True)
+  print(ledDict)
+
+  toggleClusterColorGroup(1,'g',False)
+  toggleClusterColorGroup(2,'g',False)
+  toggleClusterColorGroup(3,'g',False)
+  toggleClusterColorGroup(4,'g',False)
+  print(ledDict)
+
+  toggleClusterColorGroup(1,'y',False)
+  toggleClusterColorGroup(2,'y',False)
+  toggleClusterColorGroup(3,'y',False)
+  toggleClusterColorGroup(4,'y',False)
+  print(ledDict)
+
+  toggleClusterColorGroup(1,'r',False)
+  toggleClusterColorGroup(2,'r',False)
+  toggleClusterColorGroup(3,'r',False)
+  toggleClusterColorGroup(4,'r',False)
+  print(ledDict)
+
+  toggleClusterColorGroup(2,'y',True)
+  toggleNextLed(False)
+  toggleNextLed(True)
+  toggleNextLed(True)
+  toggleNextLed(False)
+  toggleClusterColorGroup(1,'y',True)
+  toggleNextLed(False)
+  toggleClusterColorGroup(3,'r',True)
+  toggleNextLed(False)
+  toggleClusterColorGroup(2,'g',True)
+  allOn()
+  selectClusterOFF(2)
+  selectClusterOFF(4)
+  selectClusterOFF(3)
+  toggleNextLed(True)
+  selectClusterOFF(1)
