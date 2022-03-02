94571a03fb28a19faaa785865e4370a25b889c3e
Tommy Gao <gaotommy10@gmail.com>
2022-03-01 20:49:39 -0500

    added buzzer complete sounds and such

--- pomodorotree.py
+++ pomodorotree.py
@@ -1,2 +1,9 @@
 # This is the Flexible Pomodoro Tree official code
+from pomodoro import *
 
+def buzzUp():
+  playList([(C4, 0.25), (E4, 0.25), (G4, 0.25), (C5, 0.25)])
+  
+def buzzDown():
+  playList([(C5, 0.25), (G4, 0.25), (E4, 0.25), (C4, 0.25)])
+  
