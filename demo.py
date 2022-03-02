f0734f4c187681bedbc959fd0ad1288f9f6273aa
Tommy Gao <gaotommy10@gmail.com>
2022-03-01 20:53:14 -0500

    added buzzer #14

--- demo.py
+++ demo.py
@@ -3,4 +3,18 @@
 from buzzer import *
 
 def buzz():
-  print("buzz")
+  # Set frequency as a variable
+  C7 = 2093
+  # creating a list of tuples containing frequencies and duration
+  scale = [(1319, 1), (1397, 1), (1568, 1), (1760, 1), (1976, 1), (C7, 1)]
+  # Run setup code to start hardware PWM
+  buzzerSetup()
+  # Play 4000 Hz for 1 second
+  playTime(1)
+  # Then using different methods, play a scale
+  playFreq(1047) # Frequency for C6
+  time.sleep(1) # Wait one second
+  playStop() # Stop playing after one second
+  playFreqTime(1175, 1) # Play D6 for one second
+  playList(scale) # play E6, F6, G6, A6, B6, and C7 for one second each.
+  
