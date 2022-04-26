a10726dc1f7487f1be6943e5751644932905835f
Shahzeb Naseer <78505233+shahzebnaseer@users.noreply.github.com>
2022-04-26 03:04:24 -0400

    Update pomodorotree.py

--- pomodorotree.py
+++ pomodorotree.py
@@ -171,7 +171,7 @@ def selection():
             if mode==0:
                 display.clear()
                 display.text("Select Mode:\n > Pomodoro \n    Task \n    Budget", 20,0,12)
-                print("========")
+                print("==========")
                 print("> Pomodoro \n  Task \n  Budget")
                       
 #                 display.draw_line(0, 45, 127 ,45)
@@ -191,7 +191,7 @@ def selection():
             elif mode == 1:
                 display.clear()
                 display.text("Select Mode:\n    Pomodoro \n > Task \n    Budget", 20,0,12)
-                print("========")
+                print("==========")
                 print("  Pomodoro \n> Task \n  Budget")
 #                 display.draw_line(0, 45, 127 ,45)
 #                 display.text("Select Mode", 30,45,12)
@@ -209,7 +209,7 @@ def selection():
             elif mode == 2:
                 display.clear()
                 display.text("Select Mode:\n    Pomodoro \n    Task \n > Budget", 20,0,12)
-                print("========")
+                print("==========")
                 print("  Pomodoro \n  Task \n> Budget")
 #                 display.draw_line(0, 45, 127 ,45)
 #                 display.text("Select Mode", 30,45,12)
