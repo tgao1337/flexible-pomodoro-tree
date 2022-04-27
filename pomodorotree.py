a91ad706086e6eb025f62574fe1ce69c2f684a81
Shahzeb Naseer <78505233+shahzebnaseer@users.noreply.github.com>
2022-04-27 16:28:49 -0400

    Update pomodorotree.py

--- pomodorotree.py
+++ pomodorotree.py
@@ -63,7 +63,7 @@ def checkReset(): # Done for day button
 
     while True:
         waitButton(pinB)
-        time.sleep(0.1)
+        time.sleep(0.2)
         resetRequired = not resetRequired
         
         if resetRequired:
