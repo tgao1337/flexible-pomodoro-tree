606f0e7c10e923c922f6b95d474815d5e761c0d9
Shahzeb Naseer <78505233+shahzebnaseer@users.noreply.github.com>
2022-04-28 01:47:53 -0400

    Update pomodorotree.py

--- pomodorotree.py
+++ pomodorotree.py
@@ -175,25 +175,25 @@ def updateDisplay():
             with canvas(device) as draw:
                 draw.line((0, 45, 127 ,45), fill="white")
                 if mode == "POMODORO":
-                    draw.text((30,45), "P | Settings", font=fontSmall, fill="white")  # Removed cycles
+                    draw.text((31,45), "P | Settings", font=fontSmall, fill="white")  # Removed cycles
                     draw.text((25, 0), "Set Work Time:", font=fontSmall, fill="white")
-                    draw.text((30, 10), "00:25:00", font=fontBig, fill="white") #TODO Timing conversion printing
+                    draw.text((25, 10), "00:25:00", font=fontBig, fill="white") #TODO Timing conversion printing
                 if mode == "TASK":
-                    draw.text((30,45), "T | Settings", font=fontSmall, fill="white")
+                    draw.text((31,45), "T | Settings", font=fontSmall, fill="white")
                     draw.text((40, 0), "Set Tasks:", font=fontSmall, fill="white")
                     draw.text((60, 10), "8", font=fontBig, fill="white") #TODO Task count manager
                 if mode == "BUDGET":
-                    draw.text((30,45), "B | Settings", font=fontSmall, fill="white")
+                    draw.text((31,45), "B | Settings", font=fontSmall, fill="white")
                     draw.text((25, 0), "Set Break Time:", font=fontSmall, fill="white")
-                    draw.text((30, 15), "00:55:00", font=fontBig, fill="white") # TODO Budget break timing
+                    draw.text((25, 15), "00:55:00", font=fontBig, fill="white") # TODO Budget break timing
                 
         if state == "MODE_SETTINGS_2":
             with canvas(device) as draw:
                 draw.line((0, 45, 127 ,45), fill="white")
                 if mode == "POMODORO":
-                    draw.text((30,45), "P | Settings", font=fontSmall, fill="white")  # Removed cycles
+                    draw.text((31,45), "P | Settings", font=fontSmall, fill="white")  # Removed cycles
                     draw.text((25, 0), "Set Break Time:", font=fontSmall, fill="white")
-                    draw.text((30, 10), "00:05:00", font=fontBig, fill="white") #TODO Timing conversion printing
+                    draw.text((25, 10), "00:05:00", font=fontBig, fill="white") #TODO Timing conversion printing
             
             
          
