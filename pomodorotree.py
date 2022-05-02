23f78cb82b5ce7ea6802f6454629eed8619663d3
Shahzeb Naseer <78505233+shahzebnaseer@users.noreply.github.com>
2022-05-02 00:01:20 -0400

    Update pomodorotree.py

--- pomodorotree.py
+++ pomodorotree.py
@@ -181,6 +181,7 @@ def pomoRun():
                     
                     timeTillNextLed = timeLeft // getAvailable()
                     prevTimeTillNex = timeTillNextLed
+                    print("CHANGES MADE:", timeTillNextLed, prevTimeTillNex)
                     settingsChanged = False
                     
 
