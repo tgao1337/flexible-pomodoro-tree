59aa4942f52b5c08120b0aeeb807b4d218dc88ff
Shahzeb Naseer <78505233+shahzebnaseer@users.noreply.github.com>
2022-05-02 02:55:57 -0400

    Update pomodorotree_v2.py

--- pomodorotree_v2.py
+++ pomodorotree_v2.py
@@ -174,16 +174,17 @@ def pomoRun():
                 if state == "MODE_SELECT" or state == "MODE_SETTINGS" or state == "MODE_SETTINGS_2":
                     endTime = time.time() + (pomoWorkTime - timeElapsed)
                     timeRemaining = endTime - time.time()
+                    print("IN SETTINGS", endTime)
 #                     x = time.gmtime(endTime)
 #                     displayTime = time.strftime("%H:%M:%S", x)
 #                     print("end time:", displayTime)
                     
                 
                 if state == "RUN" or (prevState == "RUN" and not state == "PAUSE"):
-                    print("IN RUN")
                     prevState = "RUN"
                     timeRemaining = endTime - time.time()
                     timeElapsed = pomoWorkTime - timeRemaining
+                     print("IN RUN", endTime)
                     
                 elif state == "PAUSE" or (prevState == "PAUSE" and not state == "RUN"): 
                     print("IN PAUSE")
