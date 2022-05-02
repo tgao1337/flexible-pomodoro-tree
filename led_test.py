a8c2b0c36ab0457281cee915b4601a1932002ef5
Shahzeb Naseer <78505233+shahzebnaseer@users.noreply.github.com>
2022-05-02 13:50:50 -0400

    Update led_test.py

--- led_test.py
+++ led_test.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
   for i in range(32):
    toggleNextLed(False,1)
   '''
-  toggleNextLed( True,16)
+  toggleNextLed( True,24)
 #   time.sleep(3)
 #   toggleNextLed(True,8)
 #   time.sleep(3)
