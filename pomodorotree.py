1a84fe4fb71471fca42673d8f8ed5a827ccf9da2
Shahzeb Naseer <78505233+shahzebnaseer@users.noreply.github.com>
2022-05-02 01:28:28 -0400

    Update pomodorotree.py

--- pomodorotree.py
+++ pomodorotree.py
@@ -175,7 +175,7 @@ def pomoRun():
                     
                     if settingsChanged:
                    
-                        timeElapsed = time.time() - startTime
+                        timeElapsed = pomoWorkTime - (endTime - startTime)
                         a = time.gmtime(timeElapsed)
                         print("TIME ELAPSED:", time.strftime("%H:%M:%S", a))
 
