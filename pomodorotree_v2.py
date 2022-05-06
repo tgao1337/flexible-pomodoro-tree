4eff287c76839d0c5159bca7f602a6b6d6ea214c
Shahzeb Naseer <78505233+shahzebnaseer@users.noreply.github.com>
2022-05-06 00:32:10 -0400

    Update pomodorotree_v2.py

--- pomodorotree_v2.py
+++ pomodorotree_v2.py
@@ -5,34 +5,7 @@ import multiprocessing as mp
 from queue import Queue
 from flask import Flask, render_template, redirect, request
 
-
-# ============================ FLASK ============================
-taskDesc = []
-app = Flask(__name__, static_folder='assets')
-
-@app.route("/")
-def home():
-    return redirect("/templates/index")
-
-@app.route("/templates/index")
-def home_template():
-    return render_template("index.html")
-
-
-#@app.route("/templates/pomodoro", methods=['POST', 'GET'])
-#def pomodoro_template():
-#    setMode("POMODORO_W")
-#    currentPWorkTime = displayTime
-#    return render_template("pomodoro.html")
-
-@app.route("/templates/task", methods=['POST', 'GET'])
-def task_template():
-    if request.method == "POST":
-        taskDesc.append(request.form['taskDescr'])
-    return render_template("task.html", taskList=taskDesc, taskDone=taskDone, taskNum=taskNum)
-
 # ========================================================
-
 
 tree = Image.open("tree.png").resize((32,32)).convert("1")
 
@@ -587,10 +560,5 @@ p2.join()
 p3.join()
 p4.join()
 p5.join()
-
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
 
 
