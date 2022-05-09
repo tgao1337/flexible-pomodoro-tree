6bc429be4a35a6079261a43a0f095da719543895
Tommy Gao <gaotommy10@gmail.com>
2022-05-08 22:53:53 -0400

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -8,6 +8,7 @@ from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
 import spidev
+import pigpio
 
 serial = i2c(port=1, address = 0x3C)
 device = sh1106(serial)
